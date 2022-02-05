3f8d170ed9c17686774a6718e5abd12a6083fbce
frogif <skyfrog@163.com>
2022-02-05 16:13:04 +0800

    prometheus

--- src/main/java/sch/frog/sparrow/configuration/PrometheusConfiguration.java
+++ src/main/java/sch/frog/sparrow/configuration/PrometheusConfiguration.java
@@ -2,16 +2,20 @@ package sch.frog.sparrow.configuration;
 
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.prometheus.client.Counter;
+import io.prometheus.client.Histogram;
+import io.prometheus.client.Summary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import sch.frog.sparrow.prometheus.FrogMonitor;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -21,7 +25,9 @@ import javax.servlet.http.HttpServletResponse;
 @Configuration
 public class PrometheusConfiguration {
 
-    @Resource
+    private static final Logger logger = LoggerFactory.getLogger(PrometheusConfiguration.class);
+
+    @Autowired
     private PrometheusMeterRegistry meterRegistry;
 
     @Autowired
@@ -37,23 +43,77 @@ public class PrometheusConfiguration {
     }
 
     @Bean
-    public WebMvcConfigurer openTelemetryMvcConfigurer(@Qualifier("requestTotalCountCollector") final Counter counter){
+    public Histogram responseTimeCollector(){
+        return Histogram.build().name("sparrow_response_duration")
+                .labelNames("method").help("响应时间").exponentialBuckets(1, 5, 6).register(meterRegistry.getPrometheusRegistry());
+    }
+
+    @Bean
+    public Summary responseTimeSummaryCollector(){
+        return Summary.build().name("sparrow_response_duration_quantiles")
+                .labelNames("method").help("响应时间分位")
+                .quantile(0.5, 0.05)
+                .quantile(0.9, 0.01)
+                .quantile(0.99, 0.001)
+                .register(meterRegistry.getPrometheusRegistry());
+    }
+
+    @Bean
+    public WebMvcConfigurer openTelemetryMvcConfigurer(@Qualifier("requestTotalCountCollector") final Counter counter,
+                                                       @Qualifier("responseTimeCollector") final Histogram responseTimeCollector,
+                                                       @Qualifier("responseTimeSummaryCollector") final Summary responseTimeSummaryCollector){
         return new WebMvcConfigurer(){
             @Override
             public void addInterceptors(InterceptorRegistry registry){
-                // 注册 访问量统计的counter拦截器
-                registry.addInterceptor(new HandlerInterceptor(){
-                    @Override
-                    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-                        String uri = request.getRequestURI();
-                        String method = request.getMethod();
-                        counter.labels(uri, method).inc();
-                        frogMonitor.accessIncrement();
-                        return true;
-                    }
-                });
+                registry.addInterceptor(new PrometheusMonitorInterceptor(counter, frogMonitor, responseTimeCollector, responseTimeSummaryCollector));
             }
         };
+    }
+
+    private static class PrometheusMonitorInterceptor implements HandlerInterceptor {
+
+        private final ThreadLocal<Long> requestStart = new ThreadLocal<>();
+
+        private final Counter accessCounter;
+
+        private final FrogMonitor frogMonitorMBean;
+
+        private final Histogram responseHistogram;
+
+        private final Summary responseTimeSummaryCollector;
+
+        public PrometheusMonitorInterceptor(Counter accessCounter, FrogMonitor frogMonitorMBean, Histogram responseHistogram, Summary responseTimeSummaryCollector) {
+            this.accessCounter = accessCounter;
+            this.frogMonitorMBean = frogMonitorMBean;
+            this.responseHistogram = responseHistogram;
+            this.responseTimeSummaryCollector = responseTimeSummaryCollector;
+        }
+
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            String uri = request.getRequestURI();
+            String method = request.getMethod();
+            accessCounter.labels(uri, method).inc();
+            frogMonitorMBean.accessIncrement();
+            requestStart.set(System.currentTimeMillis());
+            return true;
+        }
+
+        @Override
+        public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+            try{
+                String uri = request.getRequestURI();
+                Long start = requestStart.get();
+                if(start != null){
+                    long duration = System.currentTimeMillis() - start;
+                    logger.info("response duration : {}", duration);
+                    this.responseHistogram.labels(uri).observe(duration);
+                    this.responseTimeSummaryCollector.labels(uri).observe(duration);
+                }
+            }finally {
+                requestStart.remove();
+            }
+        }
     }
 
 }
--- src/main/java/sch/frog/sparrow/controller/HelloController.java
+++ src/main/java/sch/frog/sparrow/controller/HelloController.java
@@ -1,7 +1,10 @@
 package sch.frog.sparrow.controller;
 
+import io.prometheus.client.Histogram;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,10 @@ import java.util.Random;
 public class HelloController {
 
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    @Qualifier("responseTimeCollector")
+    private Histogram responseHistogram;
 
     @RequestMapping("/hello")
     public String hello(){
@@ -30,6 +37,20 @@ public class HelloController {
         Exception e = new Exception("exception for log");
         logger.error(e.getMessage(), e);
         return "exception log";
+    }
+
+    @RequestMapping("/randomResponse")
+    public String randomResponse(){
+        this.doSomething();
+        return "success";
+    }
+
+    private void doSomething(){
+        try {
+            Thread.sleep(new Random().nextInt(10000));
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 
 }
