c1f4e6c013f77fec4c6216200194a9d7c9ae2a0a
frogif <skyfrog@163.com>
2022-02-07 23:45:10 +0800

    oom demo

--- src/main/java/sch/frog/sparrow/controller/OOMController.java
+++ src/main/java/sch/frog/sparrow/controller/OOMController.java
@@ -1,14 +1,19 @@
 package sch.frog.sparrow.controller;
 
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import sch.frog.sparrow.bean.OOMBean;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RequestMapping("/oom")
-@Controller
+@RestController
 public class OOMController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OOMController.class);
 
     @RequestMapping("/triggerOOM")
     public String triggerOOM(){
@@ -16,5 +21,18 @@ public class OOMController {
         while(true){
             objs.add(new OOMBean());
         }
+    }
+
+    private final AtomicLong total = new AtomicLong(0);
+
+    @RequestMapping("/accumulationOOM")
+    public ArrayList<OOMBean> accumulationOOM(){
+        long t = total.getAndAdd(10000);
+        logger.info("accumulation total : {}", t);
+        ArrayList<OOMBean> beans = new ArrayList<>();
+        for(int i = 0 ; i < t; i++){
+            beans.add(new OOMBean());
+        }
+        return beans;
     }
 }
